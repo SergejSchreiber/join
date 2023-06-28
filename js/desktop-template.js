1c1f9281e321656ad7dddd271d8ad55a78aac86c
Sergej Schreiber <132712491+SergejSchreiber@users.noreply.github.com>
2023-06-28 12:57:08 +0200

    first commit

---
+++ js/desktop-template.js
@@ -0,0 +1,67 @@
+class JoinHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = /*html*/ `
+    <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Join</title>
+    <link rel="stylesheet" href="../css/style.css" />
+    <link rel="stylesheet" href="../css/desktop_template.css" />
+  </head>
+  <body>
+    <div  class="side-bar">
+      <img class="join-logo" src="../assets/img/join_logo_white.png" alt="join_logo" />
+      <a href="../html/summary.html"><div class="side-menu">
+        <img src="../assets/img/summary_icon.png" alt="summary_icon" />
+        <span>Summary</span>
+      </div></a>
+      <a href="../html/board.html"><div class="side-menu">
+        <img src="../assets/img/board_icon.png" alt="board_icon" />
+        <span>Board</span>
+      </div></a>
+      <a href="../html/add_task.html"><div class="side-menu">
+        <img src="../assets/img/add_task_icon.png" alt="add_task_icon" />
+        <span>Add Task</span>
+      </div></a>
+      <a href="../html/contacts.html"><div class="side-menu">
+        <img src="../assets/img/contacts_icon.png" alt="contacts" />
+        <span>Contact</span>
+      </div></a>
+      <a href="../html/legal_notice.html"><div class="legal-notice">
+        <div class="side-menu">
+          <img src="../assets/img/info_icon.png" alt="info_icon" />
+          <span>Legal notice</span>
+        </div></a>
+      </div>
+    </div>
+
+    <div class="top-bar">
+      <span> Kanban Project Management Tool</span>
+      <div class="top-bar-icons">
+        <a href="../html/help.html"><img src="../assets/img/question_icon.png" alt="question_icon" /></a>
+        <div onclick="showLogout()">
+          <img class="ellipse" src="../assets/img/ellipse_user_icon.png" alt="ellipse_user_icon" />
+          <img class="user-icon" src="../assets/img/user_icon_default.png" alt="user_icon_default">
+        </div>
+        <div onclick="hideLogout()" class="log-out-bg" id="logOutBg"></div>
+        <button class="btn btn-logout" id="logOut">Log out</button>
+        </div>
+    </div>
+    </body>
+        `;
+  }
+}
+
+customElements.define("join-header", JoinHeader);
+
+// function to show the logout button
+function showLogout() {
+  document.getElementById("logOut").style.visibility = "visible";
+  document.getElementById("logOutBg").style.visibility = "visible";
+}
+
+function hideLogout() {
+  document.getElementById("logOut").style.visibility = "hidden";
+  document.getElementById("logOutBg").style.visibility = "hidden";
+}
---
+++ js/login.js
@@ -0,0 +1,111 @@
+// functions for changing the icon in the password input field between eye-icon and eye-icon-crossed out and change the entered password between visible and non-visible
+function showEyeIconCrossedOut() {
+  document.getElementById("eye").classList.toggle("fa-eye-slash");
+  const passwordInput = document.getElementById("password");
+  const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
+  passwordInput.setAttribute("type", type);
+}
+
+function showEyeIcon() {
+  document.getElementById("eye").style.display = "block";
+  document.getElementById("password-icon").style.display = "none";
+}
+
+// functions for showing the confirmation message after password-reset-forms were submitted
+async function sendMeEmail() {
+  document.getElementById("bg-sent-email").style.display = "block";
+  document.getElementById("sent-email").style.animation = "sent-email 0.4s ease-in-out forwards";
+}
+
+function removeSendMeEmail() {
+  document.getElementById("bg-sent-email").style.display = "none";
+}
+
+// function in Reset-Passwort window to check if entered passwords are the same
+function checkConfirmedPassword() {
+  let password = document.getElementById("password");
+  let passwordConf = document.getElementById("password-conf");
+  if (password.value === passwordConf.value) {
+    password.value = "";
+    passwordConf.value = "";
+    sendMeEmail();
+    return true;
+  } else {
+    password.value = "";
+    passwordConf.value = "";
+    alert("The entered passwords are not the same");
+    return false;
+  }
+}
+
+// functions for user registration
+const STORAGE_TOKEN = "5OOXS6IHMZ5ZRRW51702PKTI3F90E0QLEFCQMEKP"; // https://remote-storage.developerakademie.org/token-generator
+const STORAGE_URL = "https://remote-storage.developerakademie.org/item";
+
+async function setItem(key, value) {
+  const payload = { key, value, token: STORAGE_TOKEN };
+  return fetch(STORAGE_URL, { method: "POST", body: JSON.stringify(payload) }).then((res) => res.json());
+}
+
+async function getItem(key) {
+  const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
+  return fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.data) {
+        return res.data.value;
+      }
+      throw `Could not find data with key "${key}".`;
+    });
+}
+
+let users = [];
+
+async function loadUsers() {
+  try {
+    users = JSON.parse(await getItem("users"));
+  } catch (e) {
+    console.error("loading error when loading users:", e);
+  }
+}
+
+async function addUser() {
+  let user = document.getElementById("username").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  if (users.some((u) => u.user === user || u.email === email)) {
+    alert(`User with username ${user} or email ${email} already exists`);
+    return;
+  }
+
+  users.push({ user, email, password });
+  await setItem("users", JSON.stringify(users));
+
+  window.location.href = "./index.html?msg=Du hast dich erfolgreich registriert";
+}
+
+// function to delete users from users array
+async function deleteUser(index) {
+  users.splice(index, 1);
+  await setItem("users", JSON.stringify(users));
+}
+
+// function to login
+function login() {
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
+
+  let user = users.find((u) => u.email == email.value && u.password == password.value);
+  console.log(user);
+  if (user) {
+    window.location.replace("./summary.html?msg=User gefunden");
+  } else {
+    alert("User nicht gefunden");
+  }
+}
+
+function guestLogin() {
+  window.location.replace("./summary.html?msg=User gefunden");
+  
+}
