2f5a5a846da4e51a7b62506b9259ec6978341cba
Sergej Schreiber <132712491+SergejSchreiber@users.noreply.github.com>
2023-07-06 19:12:05 +0200

    summary greeting according to time of the day

--- js/summary.js
+++ js/summary.js
@@ -1,8 +1,24 @@
-function formattingDate() {
-
-const timeElement = document.querySelector('#myTime');
-  const date = new Date(timeElement.getAttribute('datetime'));
-  timeElement.textContent = date.toLocaleDateString('en-US', 
-  { month: 'long', day: 'numeric', year: 'numeric' });
-
+function initSummary() {
+  formattingDate();
+  greetingTimeOfDay();
+  loadUsers();
 }
+
+function formattingDate() {
+  let timeElement = document.getElementById("myTime");
+  let date = new Date(timeElement.getAttribute("datetime"));
+  timeElement.textContent = date.toLocaleDateString("en-US", { month: "long", day: "numeric", year: "numeric" });
+}
+
+function greetingTimeOfDay() {
+  let greeting = document.getElementById("greeting");
+  let hour = new Date().getHours();
+  if (hour < 12) {
+    greeting.textContent = "Good morning";
+  } else if (hour < 18) {
+    greeting.textContent = "Good afternoon";
+  } else {
+    greeting.textContent = "Good evening";
+  }
+}
+
